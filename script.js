07070c4766333ef348334900f070c277fa0bc27a
Haal20 <69975021+Haal20@users.noreply.github.com>
2020-09-04 17:18:08 +0200

    länkade input och knapp till DOM, samt skrev ut if()statementen

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//min script-fil
+//skapar mina variablar
+let minButton = document.getElementById("myButton");
+
+minButton.addEventListener("click", function () {
+  let minInput = document.getElementById("myInput").value;
+  if (minInput < 18 && minInput > 0) {
+    //Skriv ut att personen är för ung
+  }
+  if (minInput >= 18 && minInput >= 65) {
+    //Skriv ut att personen har rätt ålder
+  }
+  if (minInput > 65) {
+    //skriv ut att personen är för gammal
+  } else {
+    //skriv ut att personen endast får skriva sin ålder i siffror
+  }
+});
